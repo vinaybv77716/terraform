d3d2a6775581a1b8aa1622fb579014a58aa1a277
vinay <vinaybv77716@gmail.com>
2023-10-21 07:39:01 +0530

    o/p and provider

--- for-ec2/output.tf
+++ for-ec2/output.tf
@@ -1,4 +1,9 @@
 output getting_private_ip {
   value       = aws_instance.example.private_ip
-  description = "description"
+  description = "getting private ip"
+}
+
+output getting_public_ip{
+  value   =aws_instance.example.public_ip
+  description="getting public ip"
 }
--- for-ec2/provider.tf
+++ for-ec2/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.11.0"
+    }
+  }
+}
+
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-east-1"  
 }
