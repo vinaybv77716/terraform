f5743a895b695f1a14e2e2982f4e06a9054c39eb
vinaybv77716 <144867641+vinaybv77716@users.noreply.github.com>
2024-07-18 18:18:41 +0530

    Mounting ebs volume

--- Project-vpc/ec2-instance-1.sh
+++ Project-vpc/ec2-instance-1.sh
@@ -15,8 +15,13 @@ sudo apt update
 #cd highway
 
 #mv * ..
+sudo file -s /dev/xvdh
+sudo mkfs -t xfs /dev/xvdh
+sudo mkdir /myebsvol
+sudo mount /dev/xvdh /myebsvol
 
 sudo apt install docker.io -y
 sudo chown $USER /var/run/docker.sock
  docker login -u bvvinay -p Bvv@22188
 docker run -p 80:80 bvvinay/prime
+
