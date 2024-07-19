b6bc1f198aa70adb7c0d130dd228235a0d6f3f57
vinaybv77716 <144867641+vinaybv77716@users.noreply.github.com>
2024-07-19 19:47:36 +0530

    cli comand

--- Project-vpc/ec2-instance-1.sh
+++ Project-vpc/ec2-instance-1.sh
@@ -24,4 +24,4 @@ sudo apt install docker.io -y
 sudo chown $USER /var/run/docker.sock
  docker login -u bvvinay -p Bvv@22188
 docker run -p 80:80 bvvinay/prime
-
+#aws ec2 modify-volume --volume-id vol-0b165024fe370b2ed --size 50
